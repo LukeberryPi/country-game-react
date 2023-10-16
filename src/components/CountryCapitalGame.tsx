7120340c244559313c54b4b61d75a78169efe811
Luke Berry <lukeberrypi@gmail.com>
2023-10-16 03:05:07 -0300

    document new shuffle method

--- src/components/CountryCapitalGame.tsx
+++ src/components/CountryCapitalGame.tsx
@@ -6,7 +6,8 @@ declare global {
   }
 }
 
-Array.prototype.shuffle = function () {
+// knuth shuffle
+Array.prototype.shuffle = function() {
   for (let i = this.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [this[i], this[j]] = [this[j], this[i]];
