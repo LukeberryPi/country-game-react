348469e332385b92a03d14c44448624048f3a67f
Luke Berry <lukeberrypi@gmail.com>
2023-10-15 23:57:15 -0300

    implement array shuffle on load

--- src/components/CountryCapitalGame.tsx
+++ src/components/CountryCapitalGame.tsx
@@ -1,7 +1,26 @@
+import Card from "./Card";
+
 interface CountryCapitalGameProps {
   data: Record<string, string>;
 }
 
 export default function CountryCapitalGame({ data }: CountryCapitalGameProps) {
-  return <div>{JSON.stringify(data)}</div>;
+  const entries = [...Object.keys(data), ...Object.values(data)];
+
+  const shuffleArray = (arr: string[]) => {
+    const copy = [...arr];
+    for (let i = copy.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [copy[i], copy[j]] = [copy[j], copy[i]];
+    }
+    return copy;
+  };
+
+  const shuffledEntries = shuffleArray(entries);
+
+  const cardMap = shuffledEntries.map((entry) => {
+    return <Card key={entry} name={entry} />;
+  });
+
+  return <div>{cardMap}</div>;
 }
